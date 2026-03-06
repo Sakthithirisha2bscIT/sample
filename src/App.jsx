e73051023fb4b93d7310fe6830a078f28e5febed
Sakthithirisha2bscIT <thirishakutty2004@gmail.com>
2026-03-06 12:11:40 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import React from "react";
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
-import Navbar from "./com.jsx/Nav";
+
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-       <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <Home/>
     </div>
   );
 }
